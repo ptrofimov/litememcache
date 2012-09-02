7e607f3e93c3d60e78df58922f2a467c02743d5a
ptrofimov <D:\temp\batyandex>
2012-09-03 01:29:51 +0400

    tests for different types

--- TinyMemcacheClient.class.php
+++ TinyMemcacheClient.class.php
@@ -1,6 +1,19 @@
 <?php
+/**
+ * TinyMemcacheClient - tiny, simple and pure-PHP alternative to Memcache and Memcached clients
+ * 
+ * @see https://github.com/memcached/memcached/blob/master/doc/protocol.txt
+ * 
+ * @author Petr Trofimov <petrofimov@yandex.ru>
+ */
 class TinyMemcacheClient
 {
+	const REPLY_STORED = 'STORED'; // Reply to storage commands: to indicate success
+	const REPLY_NOT_STORED = 'NOT_STORED'; // Reply to storage commands: to indicate the data was not stored, but not because of an error
+	const REPLY_EXISTS = 'EXISTS'; // Reply to storage commands: to indicate that the item you are trying to store with a "cas" command has been modified since you last fetched it
+	const REPLY_NOT_FOUND = 'NOT_FOUND'; // Reply to storage commands: to indicate success
+	
+
 	private $_socket;
 	
 	public function __construct( $server )
@@ -8,9 +21,10 @@ class TinyMemcacheClient
 		$this->_socket = stream_socket_client( $server );
 	}
 	
-	public function set( $key, $value, $exptime = 0, $flags = 0 )
+	public function set( $key, $value, $exptime = 0, $flags = 0, $noreply = null )
 	{
-		$cmd = sprintf( 'set %s %d %d %d' . "\r\n", $key, $flags, $exptime, strlen( $value ) );
+		$cmd = sprintf( 'set %s %d %d %d%s' . "\r\n", $key, $flags, $exptime, strlen( $value ), 
+			isset( $noreply ) ? ' 1' : '' );
 		$cmd .= $value . "\r\n";
 		fwrite( $this->_socket, $cmd );
 		$line = fgets( $this->_socket );
@@ -19,21 +33,30 @@ class TinyMemcacheClient
 	
 	public function get( $key )
 	{
-		$cmd = sprintf( 'get %s' . "\r\n", $key );
+		$keys = is_array( $key ) ? $key : array( $key );
+		
+		$cmd = sprintf( 'get %s' . "\r\n", implode( ' ', $keys ) );
 		fwrite( $this->_socket, $cmd );
-		$line = fgets( $this->_socket );
-		$line = substr( $line, 0, strlen( $line ) - 2 );
-		//var_dump('line',$key,$line);
-		if ( $line == 'END' )
+		
+		$values = array();
+		
+		while ( true )
 		{
-			$value = null;
+			$line = fgets( $this->_socket );
+			$line = substr( $line, 0, strlen( $line ) - 2 );
+			
+			if ( $line == 'END' )
+			{
+				$values[] = null;
+				break;
+			}
+			else
+			{
+				list( $cmd, $key, $exp, $length ) = explode( ' ', $line );
+				$value = fread( $this->_socket, $length + 2 );
+				$values[] = substr( $value, 0, strlen( $value ) - 2 );
+			}
 		}
-		else
-		{
-			list( $cmd, $key, $exp, $length ) = explode( ' ', $line );
-			$value = fread( $this->_socket, $length + 2 );
-			$value = substr( $value, 0, strlen( $value ) - 2 );
-		}
-		return $value;
+		return is_array( $key ) ? $values : $values[ 0 ];
 	}
 }
--- TinyMemcacheClient.test.php
+++ TinyMemcacheClient.test.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * PHPUnit tests for TinyMemcacheClient
+ * 
+ * 1. Install PHPUnit: https://github.com/sebastianbergmann/phpunit/
+ * 2. Run: phpunit TinyMemcacheClient.test.php
+ * 
  * @author Petr Trofimov
  */
 require_once ( 'TinyMemcacheClient.class.php' );
@@ -12,8 +17,29 @@ class TinyMemcacheClientTest extends PHPUnit_Framework_TestCase
 		
 		$this->assertSame( 'TinyMemcacheClient', get_class( $client ) );
 		
-		$this->assertSame( 'STORED', $client->set( 'key', 'string' ) );
+		$this->assertSame( $client::REPLY_STORED, $client->set( 'key', 'string' ) );
 		$this->assertSame( 'string', $client->get( 'key' ) );
+		
+		$this->assertSame( $client::REPLY_STORED, $client->set( 'key', 1 ) );
+		$this->assertSame( '1', $client->get( 'key' ) );
+		
+		$this->assertSame( $client::REPLY_STORED, $client->set( 'key', 0 ) );
+		$this->assertSame( '0', $client->get( 'key' ) );
+		
+		$this->assertSame( $client::REPLY_STORED, $client->set( 'key', 3.14 ) );
+		$this->assertSame( '3.14', $client->get( 'key' ) );
+		
+		$this->assertSame( $client::REPLY_STORED, $client->set( 'key', 0.0 ) );
+		$this->assertSame( '0', $client->get( 'key' ) );
+		
+		$this->assertSame( $client::REPLY_STORED, $client->set( 'key', true ) );
+		$this->assertSame( '1', $client->get( 'key' ) );
+		
+		$this->assertSame( $client::REPLY_STORED, $client->set( 'key', false ) );
+		$this->assertSame( '', $client->get( 'key' ) );
+		
+		$this->assertSame( $client::REPLY_STORED, $client->set( 'key', null ) );
+		$this->assertSame( '', $client->get( 'key' ) );
 		
 		$this->assertSame( null, $client->get( 'wrong-key' ) );
 	}
