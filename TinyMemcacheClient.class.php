c32d6726a765f15a6b5bb0fda4db5ecef704550a
ptrofimov <ptrofimov@rbc.ru>
2012-09-05 16:01:39 +0400

    sorted methods in class, tests OK

--- TinyMemcacheClient.class.php
+++ TinyMemcacheClient.class.php
@@ -26,39 +26,9 @@ class TinyMemcacheClient
 			'SERVER_ERROR' => null );
 	}
 	
-	public function getLastReply()
+	public function add( $key, $value, $exptime = 0, $flags = 0 )
 	{
-		return $this->_lastReply;
-	}
-	
-	private function _readLine()
-	{
-		$line = fgets( $this->_socket );
-		$this->_lastReply = substr( $line, 0, strlen( $line ) - 2 );
-		$words = explode( ' ', $this->_lastReply );
-		$result = isset( $this->_replies[ $words[ 0 ] ] ) ? $this->_replies[ $words[ 0 ] ] : $words;
-		if ( is_null( $result ) )
-		{
-			throw new Exception( $this->_lastReply );
-		}
-		return ( is_array( $result ) && count( $result ) == 1 ) ? reset( $result ) : $result;
-	}
-	
-	public function query( $query )
-	{
-		$query = is_array( $query ) ? implode( "\r\n", $query ) : $query;
-		fwrite( $this->_socket, $query . "\r\n" );
-		return $this->_readLine();
-	}
-	
-	public function set( $key, $value, $exptime = 0, $flags = 0 )
-	{
-		return $this->query( array( "set $key $flags $exptime " . strlen( $value ), $value ) );
-	}
-	
-	public function cas( $key, $value, $cas, $exptime = 0, $flags = 0 )
-	{
-		return $this->query( array( "cas $key $flags $exptime " . strlen( $value ) . " $cas", $value ) );
+		return $this->query( array( "add $key $flags $exptime " . strlen( $value ), $value ) );
 	}
 	
 	public function append( $key, $value )
@@ -66,34 +36,19 @@ class TinyMemcacheClient
 		return $this->query( array( "append $key 0 0 " . strlen( $value ), $value ) );
 	}
 	
-	public function prepend( $key, $value )
+	public function cas( $key, $value, $cas, $exptime = 0, $flags = 0 )
 	{
-		return $this->query( array( "prepend $key 0 0 " . strlen( $value ), $value ) );
-	}
-	
-	public function add( $key, $value, $exptime = 0, $flags = 0 )
-	{
-		return $this->query( array( "add $key $flags $exptime " . strlen( $value ), $value ) );
-	}
-	
-	public function replace( $key, $value, $exptime = 0, $flags = 0 )
-	{
-		return $this->query( array( "replace $key $flags $exptime " . strlen( $value ), $value ) );
-	}
-	
-	public function del( $key )
-	{
-		return $this->query( "delete $key" );
-	}
-	
-	public function incr( $key, $value = 1 )
-	{
-		return $this->query( "incr $key $value" );
+		return $this->query( array( "cas $key $flags $exptime " . strlen( $value ) . " $cas", $value ) );
 	}
 	
 	public function decr( $key, $value = 1 )
 	{
 		return $this->query( "decr $key $value" );
+	}
+	
+	public function del( $key )
+	{
+		return $this->query( "delete $key" );
 	}
 	
 	public function flushAll( $exptime = 0 )
@@ -116,5 +71,50 @@ class TinyMemcacheClient
 			$words = $this->_readLine();
 		}
 		return is_array( $key ) ? $keys : reset( $keys );
+	}
+	
+	public function getLastReply()
+	{
+		return $this->_lastReply;
+	}
+	
+	public function incr( $key, $value = 1 )
+	{
+		return $this->query( "incr $key $value" );
+	}
+	
+	public function prepend( $key, $value )
+	{
+		return $this->query( array( "prepend $key 0 0 " . strlen( $value ), $value ) );
+	}
+	
+	public function query( $query )
+	{
+		$query = is_array( $query ) ? implode( "\r\n", $query ) : $query;
+		fwrite( $this->_socket, $query . "\r\n" );
+		return $this->_readLine();
+	}
+	
+	public function replace( $key, $value, $exptime = 0, $flags = 0 )
+	{
+		return $this->query( array( "replace $key $flags $exptime " . strlen( $value ), $value ) );
+	}
+	
+	public function set( $key, $value, $exptime = 0, $flags = 0 )
+	{
+		return $this->query( array( "set $key $flags $exptime " . strlen( $value ), $value ) );
+	}
+	
+	private function _readLine()
+	{
+		$line = fgets( $this->_socket );
+		$this->_lastReply = substr( $line, 0, strlen( $line ) - 2 );
+		$words = explode( ' ', $this->_lastReply );
+		$result = isset( $this->_replies[ $words[ 0 ] ] ) ? $this->_replies[ $words[ 0 ] ] : $words;
+		if ( is_null( $result ) )
+		{
+			throw new Exception( $this->_lastReply );
+		}
+		return ( is_array( $result ) && count( $result ) == 1 ) ? reset( $result ) : $result;
 	}
 }
